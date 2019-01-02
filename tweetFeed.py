ac44640a3a24c436022cb1e37eace8157b4be838
Ginny Gao <qinqingao1a@hotmail.com>
2019-01-01 20:55:37 -0500

    Add files via upload

---
+++ tweetFeed.py
@@ -0,0 +1,51 @@
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import json
+import sqlite3
+from unidecode import unidecode
+
+
+conn = sqlite3.connect('twitter.db')
+c = conn.cursor()
+
+def create_table():
+	c.execute("CREATE TABLE IF NOT EXISTS CAtweets(unix REAL, tweet TEXT)")
+	conn.commit()
+create_table()
+
+
+ckey = 'tTVzDuIq4E8eeyzTOiyGl13ot'
+csecret = 'QSBDZyzYHcZvxsyJG4sREX2TekCcvwTHmdTLPmdlofLnrVMORL'
+atoken = '351859770-mhjmvAs4l3s027lUuGVxt6bewqjiRDYgS8To9AIx'
+asecret = 'tZdYT9WUhz6sinVdTJZ8hUmgOQKRAunnIiR4NRSZxYxj4'
+
+class listener(StreamListener):
+
+	def on_data(self, data):
+		try:
+			data = json.loads(data)
+			tweet = unidecode(data['text'])
+			time_ms = data['timestamp_ms']
+			print(time_ms, tweet)
+			c.execute("INSERT INTO CAtweets (unix, tweet) VALUES (?, ?)", (time_ms, tweet))
+			conn.commit()
+
+		except KeyError as e:
+			print(str(e))
+
+		return True
+
+
+	def on_error(self, status):
+		print(status)
+
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+twitterStream = Stream(auth, listener())
+twitterStream.filter(track = ['#techacquisition', '#tech', '#technology', '#merger'])
+
+
+
+
