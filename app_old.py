40395b4f89cc35423742d55eb6878d97b24debef
Ginny Gao <qinqingao1a@hotmail.com>
2019-01-04 21:31:59 -0500

    Update app_old.py

--- app_old.py
+++ app_old.py
@@ -18,7 +18,7 @@ app = dash.Dash(__name__)
 server = app.server
 
 
-mapbox_access_token = "pk.eyJ1IjoiZ2lubnlxZyIsImEiOiJjam9zcnlsemwwZHZrM3JvOTZudm5uY2E3In0.LhMpmoHGbUjWc6wmypE9cg" #fill this field with your Mapbox key
+mapbox_access_token = "" #fill this field with your Mapbox key
 
 raw = pd.read_csv('https://raw.githubusercontent.com/ginnyqg/dashboard/master/acquisitions.csv')
 
